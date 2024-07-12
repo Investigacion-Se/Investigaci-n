25200061efabc074d42c38f3de14a4f5bfb65327
Juan Biancuzzo <72982017+JuanBiancuzzo@users.noreply.github.com>
2024-07-12 10:35:03 -0300

    vault backup: 2024-07-12 10:35:03

--- _scripts/templater/onRename.js
+++ _scripts/templater/onRename.js
@@ -2,6 +2,19 @@
 async function onRename(file, oldPath) {
     console.log(file);
     console.log(oldPath);
+
+    if (esCarpeta(file)) {
+        // Actualizar indice de esta carpeta
+
+
+    } else {
+        // Si es indice, si se cambio el nombre, entonces cambiar el nombre de la carpeta
+        
+    }
+}
+
+function esCarpeta(file) {
+    return file.children;
 }
 
 module.exports = onRename;
