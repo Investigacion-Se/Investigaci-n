20c379d1df0da8c13bd021a33bc4d3193834a688
Juan Biancuzzo <72982017+JuanBiancuzzo@users.noreply.github.com>
2024-07-11 22:36:13 -0300

    vault backup: 2024-07-11 22:36:13

--- _scripts/templater/onRename.js
+++ _scripts/templater/onRename.js
@@ -1,5 +1,19 @@
 // onRename(file: TAbstractFile, oldPath: string) => any
 function onRename(file, oldPath) {
+
+    if (typeof(file) === "TFolder" || !file.parent || file.parent.isRoot()) {
+        return;
+    }    
+    let oldName = oldPath.split("/").pop();
+
+    let cambiaNombre = file.name !== oldName;
+
+
+    const dv = app.plugins.plugins["dataview"].api;
+    const archivo = dv.page(file.path);
+
+    
+
     /*
     if file es archivo {
         hay que ver
