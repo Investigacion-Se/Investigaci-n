7b6d994621ceb930e86ded6d8b0b62cfcb64dc3f
Juan Biancuzzo <72982017+JuanBiancuzzo@users.noreply.github.com>
2024-07-12 12:49:20 -0300

    vault backup: 2024-07-12 12:49:20

--- _scripts/templater/onRename.js
+++ _scripts/templater/onRename.js
@@ -10,13 +10,65 @@ async function onRename(file, oldPath) {
     console.log(`Es carpeta: ${esCarpeta ? "si" : "no"}`);
     console.log(`Cambia nombre: ${cambiaNombre ? "si" : "no"}`);
 
-    if (esCarpeta) {
-        console.log("Es carpeta");
+    if (esCarpeta || file.basename != "index") {
+        console.log(esCarpeta ? "Es carpeta" : "Es un archivo index");
         return;
+    }
+
+    let carpeta = file.path.replace(`/${file.name}`, "");
+    let indices = dv.pages(`"${carpeta}" and #Índice`)
+        .filter(ind => ind.file.folder == carpeta);
+        
+    let indice = indices.find(ind => ind.file.name == file.basename);
+    if (!indice.tags || !indice.tags.includes("Índice")) {
+        console.log("No es indice");
+        return;
+    }
+
+    if (!file.parent || file.parent.isRoot()) {
+        console.log("Se cambio el indice al root");
+        await app.vault.rename(file, oldPath);
+
+        const mensaje = "El indice se movio al root, volviendo a su lugar anterior";
+        console.log(mensjae);
+        new Notice(mensaje);
+
+        return;
+    }
+
+    let temaPadre = file.parent.path.split("/").pop();
+    let temaActual = file.basename;
+
+    if (temaPadre == temaActual) {
+        console.log("Tiene el nombre correcto, no se tiene que hacer nada");
+        return;
+    }
+
+    if (cambiaNombre) {
+        // Cambio el nombre
+        console.log("Cambiar el nombre de la carpeta");
+
+        let pathNuevo = file.parent.path.split("/");
+        pathNuevo.pop();
+        pathNuevo.push(temaActual);
+        pathNuevo = pathNuevo.join("/");
+
+        await app.vault.rename(file.parent, pathNuevo);
+
+    } else {
+        // Cambio el path
+        console.log("Cambiar el nombre del archivo");
+
+        let pathNuevo = file.path.split("/");
+        pathNuevo.pop();
+        pathNuevo.push(`${temaPadre}.md`);
+        pathNuevo = pathNuevo.join("/");
+
+        await app.vault.rename(file, pathNuevo);
     }
 }
 
-/**
+/*
 async function onRename(file, oldPath) {
     console.log(file);
     console.log(oldPath);
