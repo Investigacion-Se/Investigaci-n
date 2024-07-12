ebb9255eca5da3ec91a77177eb9e6c894e5a49fc
Juan Biancuzzo <72982017+JuanBiancuzzo@users.noreply.github.com>
2024-07-11 21:25:50 -0300

    vault backup: 2024-07-11 21:25:50

--- _scripts/templater/onRename.js
+++ _scripts/templater/onRename.js
@@ -1,6 +1,12 @@
 // onRename(file: TAbstractFile, oldPath: string) => any
 function onRename(file, oldPath) {
-    
+    /*
+    if file es archivo {
+        hay que ver
+         * Si el path actual, solo difiere en el nombre
+         * Si es indice       
+    }
+    */
 }
 
 module.exports = onRename;
